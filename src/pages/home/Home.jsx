a165a532221b73f7c5f43cb63b9a24f77a138649
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-08-23 19:01:24 +0500

    Progress Bar component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widgets/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/charts/Chart";
 
 const Home = () => {
   return (
@@ -14,6 +16,10 @@ const Home = () => {
           <Widget type="order" />
           <Widget type="earning" />
           <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart />
         </div>
       </div>
     </div>
